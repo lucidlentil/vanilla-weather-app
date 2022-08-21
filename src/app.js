13e59ec154b25eee0fda47b62d824e7bd5349feb
lucidlentil <greta.gabriela@gmail.com>
2022-08-21 20:31:47 +1200

    fixed icon

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 let apiKey = "74ca054ff55e08af1a7a77572c080cfe"; 
-let apiUrl =`https://api.openweathermap.org/data/2.5/weather?q=New York&appid=${apiKey}&units=metric`; 
+let city = "auckland";
+let apiUrl =`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`; 
 
 
 
@@ -27,12 +28,16 @@ function displayTemperature(response) {
     let humidityElement = document.querySelector("#humidity");
     let windElement = document.querySelector("#wind");
     let dateElement = document.querySelector("#date"); 
+    let iconElement = document.querySelector("#icon"); 
+
     temperatureElement.innerHTML = Math.round(response.data.main.temp); 
     cityElement.innerHTML = response.data.name; 
     descriptionElement.innerHTML = response.data.weather[0].description; 
     humidityElement.innerHTML = response.data.main.humidity; 
     windElement.innerHTML = Math.round(response.data.wind.speed); 
-    dateElement.innerHTML = formatDate(response.data.dt * 1000)
+    dateElement.innerHTML = formatDate(response.data.dt * 1000);
+    iconElement.setAttribute("src",`http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
+    iconElement.setAttribute("alt", response.data.weather[0].description); 
 }
 
 axios.get(apiUrl).then(displayTemperature);
